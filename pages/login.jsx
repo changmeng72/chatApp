e4b8f3e7ef516bdf55665bde767fc1d52c0309bf
Meng Chang <changmeng72@gmail.com>
2021-01-23 10:37:34 -0500

    Login page update to support small screen

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,65 +1,88 @@
-import { Formik, Form ,Field} from 'formik';
-import { Button, Box ,Container,Grid,Paper} from '@material-ui/core'
-import { TextField } from 'formik-material-ui';
-import LoginBox from '../components/LoginBox'
+import { Formik, Form, Field } from "formik";
+import {
+  Button,
+  Box,
+  Container,
+  Grid,
+  Paper,
+  useMediaQuery,
+  makeStyles,
+} from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import LoginBox from "../components/LoginBox";
+import { useEffect } from "react";
 //import bgImg from url(${bgImg})
 //import chatImg from '../assets/chat.png'
 
- 
-const styles =  {
-    root: {
-        height: '560px',
-        width: '800px',
-        backgroundImage: 'url("/assets/bg-img.png")',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-        marginTop: '70px',
-        display: 'flex'
-    },
-    box1:{
-        height: '560px',
-        width: '340px',
-        backgroundImage: `linear-gradient(rgba(58,144,255,0.85),rgba(134,185,255,0.85))`,
-        fontFamily: "'Open Sans', sans-serif",
-        fontWeight: 400,
-        textAlign: 'center',
-        fontSize: '20px', 
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent:"center"
-         
-    },
-    box2:{
-        height: '560px',
-        width: '460px',
-        fontSize: '40px',
-        
-    }
-    
 
-};
- 
- 
-export default function Login() {    
-    
-    return <>
-         <Container maxWidth='md'>
-            <Paper style = {styles.root}>                
-                 
-                    <Box style = {styles.box1} >  
-                        <Box style={{ color: 'white', }}>...</Box>
-                        <Box style={{ color: 'white', }}>Conversation with anyone</Box>
-                        <Box style={{ color: 'white', }}>with any language</Box>
-                   </Box>
-                    <Box style = {styles.box2}>
-                    <LoginBox /> 
-                    </Box>
-                 
-            </Paper> 
-               
-       </Container>                
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    marginTop: "10px",
+    display: "flex",
+  },
+  box1: {
+    width: "41.5%",
+    position: "relative",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  box3: {
+    height: "100%",
+    width: "100%",
+    fontFamily: "'Open Sans', sans-serif",
+    fontWeight: 400,
+    textAlign: "center",
+    fontSize: "22px",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+
+    zIndex: 1,
+    top: 0,
+    left: 0,
+    position: "absolute",
+    backgroundImage: `linear-gradient(rgba(58,144,255,0.85),rgba(134,185,255,0.85))`,
+  },
+  box2: {
+    width: "58.5%",
+    fontSize: "40px",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+  },
+  sideImage: {
+    width: "100%",
+    height: "100%",
+  },
+}));
+
+export default function Login() {
+  const matches = useMediaQuery("(max-width:600px)");
+  const styles = useStyles();
+  return (
+    <>
+      <Container maxWidth="md">
+        <Paper className={styles.root}>
+          <Box className={styles.box1}>
+            <img
+              src="http://127.0.0.1:3000/assets/bg-img.png"
+              className={styles.sideImage}
+            ></img>
+            <Box className={styles.box3}>
+              <Box style={{ color: "white" }}>...</Box><br></br>
+              <Box style={{ color: "white" }}>Converse with anyone</Box>
+                          <Box style={{ color: "white" }}>with any language</Box>
+                          <br></br><br></br>
+            </Box>
+          </Box>
+          <Box className={styles.box2}>
+            <LoginBox />
+          </Box>
+        </Paper>
+      </Container>
     </>
-    
+  );
 }
-            
